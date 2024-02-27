d312ce61ec0a2a084d1b06ed9d9b2da29ea31004
Krishi Wali <krishiwali@hotmail.co.uk>
2024-02-27 16:26:36 +0000

    Add a proper response from statusCallback

--- functions/statusCallback.protected.js
+++ functions/statusCallback.protected.js
@@ -1,3 +1,6 @@
 export function handler (context, event, callback) {
     // Do stuff
+    console.log(context, event, callback)
+    const response = new Response();
+    return callback(null, response)
 }
