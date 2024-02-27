c13c0b684405f29d62f359dcc9b605c386ba9fc3
Krishi Wali <krishiwali@hotmail.co.uk>
2024-02-27 20:19:49 +0000

    Small refactor

--- functions/transcribe.protected.js
+++ functions/transcribe.protected.js
@@ -2,10 +2,12 @@ import { twiml, Response } from 'twilio';
 import moment from 'moment';
 
 export function handler (context, event, callback) {
+    // Create a Twilio Response object
+    const response = new Response();
     // Create a TwiML Voice Response object to build the response
     const twiml_response = new twiml.VoiceResponse();
 
-    // If no previous conversation is present, or if the conversation is empty, start the conversation
+    // If the call has just started
     if (!event.request.cookies.initiated) {
         // Greet the user with a message using AWS Polly Neural voice
         twiml_response.say({
@@ -13,6 +15,11 @@ export function handler (context, event, callback) {
         },
             "Hey! I'm HillingdonLex, a chatbot created to help the residents of Hillingdon. What would you like to talk about today? I could help you order recycling bags, report a street that needs cleaning, request a housing repair or make an adult social care query among other things."
         );
+
+        const callStartTimestamp = encodeURIComponent(moment().utc().format('ddd, DD MMM YYYY HH:mm:ss ZZ'));
+
+        response.setCookie('initiated', true, ['Path=/']);
+        response.setCookie('callStartTimestamp', callStartTimestamp, ['Path=/']);
     }
 
     // Listen to the user's speech and pass the input to the /respond Function
@@ -23,22 +30,11 @@ export function handler (context, event, callback) {
         action: '/respond', // Send the collected input to /respond
     });
 
-    // Create a Twilio Response object
-    const response = new Response();
-
     // Set the response content type to XML (TwiML)
     response.appendHeader('Content-Type', 'application/xml');
 
     // Set the response body to the generated TwiML
     response.setBody(twiml_response.toString());
-
-    // If this is the beginning of the call
-    if (!event.request.cookies.initiated) {
-        const callStartTimestamp = encodeURIComponent(moment().utc().format('ddd, DD MMM YYYY HH:mm:ss ZZ'));
-
-        response.setCookie('initiated', true, ['Path=/']);
-        response.setCookie('callStartTimestamp', callStartTimestamp, ['Path=/']);
-    }
 
     // Return the response to Twilio
     return callback(null, response);
